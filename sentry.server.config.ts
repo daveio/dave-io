e89b1b7b398f4de426671b932ee470f70a14a3ff
Dave Williams <dave@dave.io>
2025-08-11 08:53:41 +0100

    🔐 Replace hardcoded Sentry DSN with environment variable
    Move sensitive Sentry DSN configuration from hardcoded values to
    environment variables in both client and server configurations. This
    improves security by preventing exposure of the DSN in source code and
    allows for different configurations across environments.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nuxt"
 
 Sentry.init({
-  dsn: "https://9d89cd983c39d62a5982f0f5cefce01b@o374595.ingest.us.sentry.io/4509822235246592",
+  dsn: process.env.SENTRY_DSN || "",
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
